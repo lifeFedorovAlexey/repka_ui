cd21c7b19303c9367a7b41f216a933bec21f5d84
Alexey <56967387+lifeFedorovAlexey@users.noreply.github.com>
2023-06-15 21:27:29 +0300

    change name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'RepkaUI',
+        name: 'repkaui',
         exec_mode: 'cluster',
         instances: 'max',
         script: './node_modules/nuxt/bin/nuxt.js',
